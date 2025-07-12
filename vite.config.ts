4fe5a9a0cf06a1a7ed8954eab79fd84743c51544
Lucas <lucasnrgaard@gmail.com>
2025-07-12 05:08:27 +0200

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-oxc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  experimental: {
+    enableNativePlugin: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
